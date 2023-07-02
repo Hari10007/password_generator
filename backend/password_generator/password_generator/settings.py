f51b0d227cbdbc37a1ccecc17d1255b046aeadd2
HARIKRISHNAN S R <111041350+Hari10007@users.noreply.github.com>
2023-07-02 20:39:03 +0530

    new

--- backend/password_generator/password_generator/settings.py
+++ backend/password_generator/password_generator/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s!v%)8a+2r!6&8a5u0opr)b_#augqx^4*)kc%)28m8k!p*ky(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -145,12 +145,8 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
