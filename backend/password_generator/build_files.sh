2d57575266b03f317ab982c4de58bde476af8a5d
HARIKRISHNAN S R <111041350+Hari10007@users.noreply.github.com>
2023-07-02 21:18:45 +0530

    change5

--- backend/password_generator/build_files.sh
+++ backend/password_generator/build_files.sh
@@ -1,3 +1,5 @@
+python3.9 --version
+python3.9 -m pip --version
 python3.9 -m pip install --upgrade pip 
 python3.9 pip install --root-user-action=ignore
 python3.9 -m pip install -r requirements.txt
