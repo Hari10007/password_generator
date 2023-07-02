ff5993b86f8600589915b74e2ce10053fba0d278
HARIKRISHNAN S R <111041350+Hari10007@users.noreply.github.com>
2023-07-02 21:14:09 +0530

    change3

--- backend/password_generator/build_files.sh
+++ backend/password_generator/build_files.sh
@@ -1,5 +1,4 @@
 python3.9 -m pip install --upgrade pip 
 python3.9 pip install --root-user-action=ignore
 python3.9 -m pip install -r requirements.txt
-python3.9 -m  pip install --force-reinstall pysqlite3
 python3.9 manage.py collectstatic --noinput --clear
