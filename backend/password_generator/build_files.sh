84617642398dcfc3d36cb8e1939022a1622c10ee
HARIKRISHNAN S R <111041350+Hari10007@users.noreply.github.com>
2023-07-02 22:42:11 +0530

    change14

--- backend/password_generator/build_files.sh
+++ backend/password_generator/build_files.sh
@@ -1,5 +1,5 @@
-python3.9 -m venv venv
-source venv/bin/activate
+# python3.9 -m venv venv
+# source venv/bin/activate
 python3.9 pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
