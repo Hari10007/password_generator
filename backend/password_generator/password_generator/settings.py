9cad2b49feacef22eff98284f5ff6bd63056a270
HARIKRISHNAN S R <111041350+Hari10007@users.noreply.github.com>
2023-07-04 13:07:48 +0530

    last

--- backend/password_generator/password_generator/settings.py
+++ backend/password_generator/password_generator/settings.py
@@ -170,11 +170,14 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles_build', 'static')
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend/password_generator/password_generator/urls.py
+++ backend/password_generator/password_generator/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
+from django.urls import include, re_path
+
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('api/',include('user.urls')),
     path('api/password_generator/', include('password.urls')),
-]
 
-urlpatterns += staticfiles_urlpatterns()
+]
